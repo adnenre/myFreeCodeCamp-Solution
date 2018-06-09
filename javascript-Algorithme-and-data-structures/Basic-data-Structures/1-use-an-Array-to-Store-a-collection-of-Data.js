338a3ae8eafbcc5287170da7f91c91bc732ab9d0
adnen rebai <adnen.rebai@gmail.com>
2018-06-09 02:59:02 +0200

    add first and second solution in basic data structures section

---
+++ javascript-Algorithme-and-data-structures/Basic-data-Structures/1-use-an-Array-to-Store-a-collection-of-Data.js
@@ -0,0 +1,8 @@
+
+
+/**
+ * solution by @adnen_rebai
+ */
+
+ // hint :  Your array should contain at least one string, one number, and one boolean.
+let yourArray =[1,'one',true,'hello','everyOne'];
---
+++ javascript-Algorithme-and-data-structures/Basic-data-Structures/2-access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * solution by @adnen_rebai
+ */
+
+ // hint : In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".
+let myArray = ["a", "b", "c", "d"];
+myArray[1]+=' is the second caracter in alphabet ;)';
+console.log(myArray);
