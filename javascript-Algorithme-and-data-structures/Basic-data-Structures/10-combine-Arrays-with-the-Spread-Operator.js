abe97e6180b169a5867b3ae540bc499ab204b478
adnen rebai <adnen.rebai@gmail.com>
2018-06-10 08:28:30 +0200

    add solution of challenge 10 using spread Operator

---
+++ javascript-Algorithme-and-data-structures/Basic-data-Structures/10-combine-Arrays-with-the-Spread-Operator.js
@@ -0,0 +1,13 @@
+
+/**
+ * Solution by @adnen_rebai
+ */
+
+ function spreadOut(){
+     let fragment = ['to', 'code'];
+     let sentense = ['learn',...fragment,'is','fun'];
+     return sentense;
+ }
+
+
+ console.log(spreadOut());
