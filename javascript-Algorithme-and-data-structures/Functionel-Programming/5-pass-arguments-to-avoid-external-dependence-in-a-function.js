b848884f57f6da869729cac3ddc2f4aa5329decb
adnen rebai <adnen.rebai@gmail.com>
2018-06-14 09:02:41 +0200

    add solution for pass-arguments-to-avoid-external-dependence-in-a-function

---
+++ javascript-Algorithme-and-data-structures/Functionel-Programming/5-pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,16 @@
+
+/**
+ * Solution by @adnen_reabi
+ */
+
+ // the global variable
+ var fixedValue = 4;
+
+ // Add your code below this line
+ function incrementer(value){
+    return value +1 ;
+ }
+
+ var newValue = incrementer(fixedValue);
+ console.log(newValue);
+ console.log(fixedValue);
