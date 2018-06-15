95a62dbd3897deb57f35c631686866b9cb347071
adnen rebai <adnen.rebai@gmail.com>
2018-06-15 15:34:31 +0200

    fix syntax on every() method

--- javascript-Algorithme-and-data-structures/Functionel-Programming/21-use-the-every-method-to-check-that-every-element-in-an-array-meets-a-criteria.js
+++ javascript-Algorithme-and-data-structures/Functionel-Programming/21-use-the-every-method-to-check-that-every-element-in-an-array-meets-a-criteria.js
@@ -6,9 +6,8 @@
 
 function checkPositive(arr) {
     
-    return arr.every((element) =>  elemtent>0);
+    return arr.every((element) =>  element>0);
    
   }
   checkPositive([1, 2, 3, -4, 5]);
 
-  
