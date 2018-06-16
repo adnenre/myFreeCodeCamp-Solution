32812e8bc5a8a988febf3e48da879bd27f8ed0a3
adnen rebai <adnen.rebai@gmail.com>
2018-06-16 13:15:28 +0200

    solution for life cycle hook challenge 32 and 33 , solution for inline style 38

---
+++ Front End librairies/react/32-use-the-lifecycle-method-componentwillMount.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * Solution by @adnen_rebai
+ */
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // change code below this line
+     console.log('this is called before rendering the component');
+      // change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
