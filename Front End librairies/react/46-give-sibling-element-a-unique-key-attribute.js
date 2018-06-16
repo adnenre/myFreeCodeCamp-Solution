9fd0eeabbbe096f6b0bf8e2deefe34430dd584d5
adnen rebai <adnen.rebai@gmail.com>
2018-06-16 17:05:25 +0200

    solution for giving sibling element a key using map

---
+++ Front End librairies/react/46-give-sibling-element-a-unique-key-attribute.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * Solution by @adnen_rebai
+ */
+
+
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map((item,index)=><li key={index}> {item}</li>); // change code here
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
