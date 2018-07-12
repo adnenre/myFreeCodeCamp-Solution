9d22acbceaac1444547cf5758a88bcefa95c2a38
adnenre <adnen.rebai@gmail.com>
2018-07-12 14:15:48 +0200

    match single caracter with multiple posibilitie using [ ]

---
+++ javascript-Algorithme-and-data-structures/Regular-Expression/8-Match-single-caracter-with-multiple-possibilities.js
@@ -0,0 +1,5 @@
+let quoteSample = "Beware of bugs in the above code; i have only proved it correct , not tried it .";
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex);
+
+console.log(result);
